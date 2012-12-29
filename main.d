1ffcc9d5d91f985af01b7fdd74349ddf6fe54b49
Owen Yamauchi <owen.yamauchi@gmail.com>
2012-12-29 22:04:32 +0100

    Add a C-code output mode, for speed comparison with gcc -O3

--- main.d
+++ main.d
@@ -5,20 +5,58 @@ import ir;
 import std.container;
 import std.outbuffer;
 import std.file;
+import std.getopt;
 import std.stdio;
 
+void codegenC(string source, ref OutBuffer buf) {
+  buf.write("#include <stdlib.h>\n");
+  buf.write("int main() {\n");
+  buf.write("char* ptr = calloc(30000, 1);\n");
+  buf.write("int getchartmp;\n");
+  foreach (ulong i; 0..source.length) {
+    switch (source[i]) {
+      case '+': buf.write("++*ptr;\n"); break;
+      case '-': buf.write("--*ptr;\n"); break;
+      case '<': buf.write("--ptr;\n"); break;
+      case '>': buf.write("++ptr;\n"); break;
+      case '.': buf.write("putchar(*ptr);\n"); break;
+      case ',': buf.write("getchartmp = getchar();");
+                buf.write("if (getchartmp != -1) *ptr = (char)getchartmp;\n");
+                break;
+      case '[': buf.write("while (*ptr) {\n"); break;
+      case ']': buf.write("}\n"); break;
+      default: // meh
+    }
+  }
+
+  buf.write("return 0;\n");
+  buf.write("}\n");
+}
+
 int main(string[] argv) {
+  bool outputC = false;
+  getopt(argv,
+      "c", &outputC
+      );
+
   if (argv.length != 2) {
-    writefln("Usage: %s <file.bf>", argv[0]);
+    writefln("Usage: %s [flags] <file.bf>", argv[0]);
+    writeln("Flags:");
+    writeln("  --c    Output C code instead of assembly");
     return 0;
   }
 
   string filename = argv[1];
   string source = cast(string) read(filename);
+  OutBuffer buf = new OutBuffer();
+
+  if (outputC) {
+    codegenC(source, buf);
+    write(buf.toString());
+    return 0;
+  }
 
   Array!Instruction instrs = parse(source);
-
-  OutBuffer buf = new OutBuffer();
   codegenInstructions(instrs, buf);
   write(buf.toString());
 
